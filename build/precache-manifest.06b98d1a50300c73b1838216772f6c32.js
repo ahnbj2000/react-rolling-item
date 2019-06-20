66007a0384e74b9ebcfc6e2549f806b22c1a6595
ahnbj2000 <ahnbj2000@naver.com>
2019-06-20 16:20:30 +0900

    changed image, npmignore

---
+++ build/precache-manifest.06b98d1a50300c73b1838216772f6c32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3074273cc30c456d5373a1cf29155ed",
+    "url": "/react-rolling-item/index.html"
+  },
+  {
+    "revision": "afbf0418143c77806378",
+    "url": "/react-rolling-item/static/css/main.bca33857.chunk.css"
+  },
+  {
+    "revision": "908738f3dd27fcd79211",
+    "url": "/react-rolling-item/static/js/2.0631f4e9.chunk.js"
+  },
+  {
+    "revision": "afbf0418143c77806378",
+    "url": "/react-rolling-item/static/js/main.964bd647.chunk.js"
+  },
+  {
+    "revision": "a173088fa2bd0e06f602",
+    "url": "/react-rolling-item/static/js/runtime~main.a8bd9757.js"
+  },
+  {
+    "revision": "9d33107d8a6a60e99f25f4777081862d",
+    "url": "/react-rolling-item/static/media/french-fries.9d33107d.png"
+  }
+]);
