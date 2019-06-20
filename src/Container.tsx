968afd34cb112d1abfcf84e938cc1a8917506717
ahnbj2000 <ahnbj2000@naver.com>
2019-06-20 16:17:53 +0900

    changed image, npmignore

--- src/Container.tsx
+++ src/Container.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import RollingItem from './rolling-item';
 
 // import imageFile from './images/spr_machine_rolling.png';
-import imageFile from './images/french-fries.png';
+import imageFile from './images/bg-fruit.png';
 
 interface IContainerProps {
 
@@ -30,23 +30,33 @@ export default class Container extends React.PureComponent<IContainerProps, ICon
           on={this.state.start}
           row={3}
           backgroundImage={imageFile}
-          backgroundSize="1280px 640px"
+          // backgroundSize="1280px 640px"
+          backgroundSize="600px 564px"
           itemInfo={
             [
-              { x: 0, y: -63, id: 'item_0' },
-              { x: -267, y: -63, id: 'item_1' },
-              { x: -536, y: -63, id: 'item_2' },
-              { x: -803, y: -63, id: 'item_3' },
-              { x: -1070, y: -63, id: 'item_4' },
-              { x: 0, y: -365, id: 'item_5' },
-              { x: -267, y: -365, id: 'item_6' },
-              { x: -536, y: -365, id: 'item_7' },
-              { x: -803, y: -365, id: 'item_8' },
-              { x: -1070, y: -365, id: 'item_9' }
+              { x: -39, y: -28, id: 'item_0' },
+              { x: -39, y: -217, id: 'item_1' },
+              { x: -39, y: -406, id: 'item_2' },
+              { x: -241, y: -28, id: 'item_3' },
+              { x: -241, y: -217, id: 'item_4' },
+              { x: -241, y: -406, id: 'item_5' },
+              { x: -437, y: -28, id: 'item_6' },
+              { x: -437, y: -217, id: 'item_7' },
+              { x: -437, y: -406, id: 'item_8' },
             ]
+            // [
+            //   { x: 0, y: 0, id: 'item_0' },
+            //   { x: -150, y: 0, id: 'item_1' },
+            //   { x: -300, y: 0, id: 'item_2' },
+            //   { x: 0, y: -190, id: 'item_7' },
+            //   { x: -150, y: -190, id: 'item_8' },
+            //   { x: -300, y: -190, id: 'item_9' }
+            // ]
           }
-          width={210}
-          height={210}
+          // width={210}
+          // height={210}
+          width={177}
+          height={181}
           completionAnimation={true}
           onProgress={(isProgress, result) => { console.log(result); }}
         />
