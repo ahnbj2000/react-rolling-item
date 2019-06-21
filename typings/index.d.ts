6c110a5a18eecd77424baf10fa946b7848746db2
ahnbj2000 <ahnbj2000@naver.com>
2019-06-21 15:42:44 +0900

    modified index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 declare module 'react-rolling-item' {
   type ItemInfo = {x: string | number, y: string | number, id?: any, intro?: boolean};
 
-  interface RollingItemProps extends React.Props<RollingItem> {
+  interface RollingItemProps {
     on: boolean;
     row: number;
     backgroundImage: string;
