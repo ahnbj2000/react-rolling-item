1f67fd75150e73bbb6aeeca3bb7054690e90962e
ahnbj2000 <ahnbj2000@naver.com>
2019-06-25 17:25:37 +0900

    added delay, intro props

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -10,6 +10,8 @@ declare module 'react-rolling-item' {
     itemInfo: ItemInfo[];
     width: number;
     height: number;
+    startDelay: number;
+    reset: boolean;
     completionAnimation?: boolean;
     onProgress?: (progress: boolean, result?: any[]) => void;
   }
